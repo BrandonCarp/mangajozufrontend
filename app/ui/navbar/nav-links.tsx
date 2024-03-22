c3e63e2a2271c77381a1b9b6dd73006e8523feb6
Brandon C <brancarp93@gmail.com>
2024-03-22 16:28:07 -0400

    small style changes to nav bar

--- app/ui/navbar/nav-links.tsx
+++ app/ui/navbar/nav-links.tsx
@@ -34,9 +34,12 @@ export default function Navbar() {
             <Link
               key={link.name}
               href={link.href}
-              className={clsx("flex flex-row  pl-5", {
-                " text-blue-500": pathname === link.href,
-              })}
+              className={clsx(
+                "flex  h-[48px] grow items-center justify-center gap-2 rounded-md  p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+                {
+                  " text-blue-500": pathname === link.href,
+                }
+              )}
             >
               <p>{link.name}</p>
             </Link>
