bea34d0e33552679133ae662103acbf7cb8b9447
Brandon <brancarp93@gmail.com>
2024-03-28 20:32:09 -0400

    jest

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import Carousel from "./ui/carousel/carousel";
-// https://www.material-tailwind.com/docs/react/carousel
+// https://nextjs.org/docs/pages/building-your-application/testing/jest
 export default function Home() {
   return (
     <div className="flex">
