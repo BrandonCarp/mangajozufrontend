243b7093af7e30545e8555b3ef3039bfe3b9f7b5
Brandon <brancarp93@gmail.com>
2024-05-12 20:33:33 -0400

    hero updates

--- app/ui/carousel/carousel.tsx
+++ app/ui/carousel/carousel.tsx
@@ -1,16 +1,17 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import CarouselItem from "./carouselItem";
 import eov from "../images/eov.jpeg";
 import img1 from "../images/img1.jpeg";
 import img2 from "../images/img2.jpeg";
+import CarouselItem from "./CarouselItem";
 import Image from "next/image";
 
 
 export default function Carousel() {
   const [currentIndex, setCurrentIndex] = useState(0);
   const images = [eov, img1, img2];
+  const imageUrls = ['eov.jpeg', 'img1.jpeg', '.img2.jpeg']
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -20,31 +21,14 @@ export default function Carousel() {
     return () => clearInterval(interval);
   }, [images]);
 
+
   return (
-    <div data-testid="main-carousel" className="relative mt-[48px] ">
-      <div className="">
-     <Image src={images[currentIndex]} layout="responsive" alt="carousel-item" 
-     className="" width={500} height={500}/>
-        {/* <CarouselItem image={images[currentIndex]} /> */}
-      </div>
+    <div data-testid="main-carousel" className="relative  mt-[48px] ">
+     
+     {/* <CarouselItem image={images[currentIndex]} /> */}
+     <Image alt="backgroundCarousel"  src={images[currentIndex]} className="w-full h-[80%]" width={500} height={500}  />
+  
     </div>
   );
 }
-   {/* {images.map((image, index) => (
-          <Image
-            key={index}
-            src={image}
-            layout="fill"
-            alt={`carousel-item${index}`}
-            className="absolute top-0 left-0 transition-opacity duration-500"
-          />
-        ))} */}
-        {/* <Image
-          src={images[currentIndex]}
-          layout="responsive"
-          alt="carousel-item"
-          className="opacity-0"
-          onLoad={(e) => {
-            (e.target as HTMLImageElement).classList.add(`opacity-100`);
-          }}
-        /> */}
+ 
--- app/ui/carousel/carouselItem.tsx
+++ app/ui/carousel/carouselItem.tsx
@@ -9,8 +9,15 @@ interface CarouselItemProps {
 
 export default function CarouselItem({ image }: CarouselItemProps) {
   return (
-    <div>
-      <Image src={image} layout="responsive" alt="carousel-item" className="" width={500} height={500}/>
-    </div>
+    <main className="flex flex-col items-center justify-between">
+      <div className="relative w-full">
+        <div className=" w-full">
+          <Image alt="backgroundCarousel"  src={image} className="w-full" width={500} height={500}  />
+        </div>
+<div>
+  <h1 className="max-w-screen-lg m-auto  py-5 px-5 my-10 text-center text-2xl rounded-2xl bg-red text-white">Hey daodajdajkda</h1>
+</div>
+      </div>
+      </main>
   );
 }
